e28aa4857b6957bdefddb0614719a8b2c6b147d6
Raf _ <btcossin@gmail.com>
2021-03-27 19:19:15 -0400

    first commit

--- Shopping/index.php
+++ Shopping/index.php
@@ -125,26 +125,58 @@ if(isset($_SESSION['cart'])){
 
 <!--!start #header -->
 
+<!--request method -->
+<?php
+$name = "";
+$nameErr = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["name"])) {
+    $nameErr = "Name is required";
+  } else {
+    $name = test_input($_POST["name"]);
+    // check if name only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z-' ]*$/",$name)) {
+      $nameErr = "Only letters and white space allowed";
+    }
+  }
+}
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+  ?>
+<!--Start search -->
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+  Name: <input type="text" name="name" value="<?php echo $name;?>">
+  <span class="error">* <?php echo $nameErr;?></span>
+  <br><br>
+  </form>
 
 <!--start #main-site -->
+
 <div class="container">
-    <div class="row text-center py-5">
+<div class="row text-center py-5">
     
     <?php
-    
+
+    $text = "";
+    /*
     $result = $database->getData();
     while($row = mysqli_fetch_assoc($result)){
         component($row['product_name'], $row['product_price'], $row['product_image'], $row['id']);
     }
+    */
     
-    /*
-    $sql = "SELECT * FROM producttb WHERE id = 4";
+    $sql = "SELECT * FROM producttb WHERE product_name LIKE '%$name%'";
     $result = mysqli_query($database->con, $sql);
 
     while($row = mysqli_fetch_assoc($result)){
       component($row['product_name'], $row['product_price'], $row['product_image'], $row['id']);
     }
-    */
+    
         ?>
     </div>
 </div>
